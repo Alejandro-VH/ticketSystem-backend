5d268167e53047dc2810cdff60422f95dd75ba0a
Alejandro-VH <84481926+Alejandro-VH@users.noreply.github.com>
2025-04-21 02:44:10 -0400

    feat(controllers): implement basic UserController and TicketController

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -6,5 +6,155 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    // Functions
+    public function CreateTicket(Request $request)
+    {
+        $messages = $this->GetMessages();
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'status' => 'required',
+            'priority' => 'required',
+        ], $messages);
+
+        $ticket = new Ticket();
+        $ticket->title = $request->title;
+        $ticket->description = $request->description;
+        $ticket->status = $request->status;
+        $ticket->priority = $request->priority;
+        $ticket->save();
+
+        return response([
+            'message' => 'Se ha creado el ticket exitosamente',
+            'data' => [$ticket],
+            'error' => false,
+        ],200);
+    }
+
+    public function UpdateTicket($id, Request $request)
+    {
+        $ticket = Ticket::find($id);
+        if (!$ticket) {
+            return response([
+                'message' => 'No se encontró el ticket indicado',
+                'data' => [],
+                'error' => true,
+            ],404);
+        }
+
+        $messages = $this->GetMessages();
+        $request->validate([
+            'title' => 'sometimes',
+            'description' => 'sometimes',
+            'status' => 'sometimes',
+            'priority' => 'sometimes',
+        ], $messages);
+
+        $ticket->update($request->all());
+        
+        return response([
+            'message' => 'Ticket actualizado exitosamente',
+            'data' => [],
+            'error' => false,
+        ],200);
+    }
+
+    public function DisableTicket($id){
+        $ticket = Ticket::find($id);
+        if (!$ticket) {
+            return response([
+                'message' => 'No se encontró el ticket indicado',
+                'data' => [],
+                'error' => true,
+            ],404);
+        }
+
+        $ticket->isEnabled = false;
+        $ticket->save();
+        return response([
+            'message' => 'Ticket deshabilitado exitosamente',
+            'data' => [],
+            'error' => false,
+        ],200);
+    }
+
+    public function EnableTicket($id){
+        $ticket = Ticket::find($id);
+        if (!$ticket) {
+            return response([
+                'message' => 'No se encontró el ticket indicado',
+                'data' => [],
+                'error' => true,
+            ],404);
+        }
+
+        $ticket->isEnabled = true;
+        $ticket->save();
+
+        return response([
+            'message' => 'Ticket deshabilitado exitosamente',
+            'data' => [],
+            'error' => false,
+        ],200);
+    }
+    
+    // Gets
+    public function GetAllTickets()
+    {
+        try {
+            $tickets = Ticket::all();
+            if ($tickets->isEmpty()){
+                return response([
+                    'message' => 'No se encontraron tickets',
+                    'data' => [],
+                    'error' => false,
+                ],404);
+            }
+            return response([
+                'message' => 'Tickets encontrados exitosamente',
+                'data' => [$tickets],
+                'error' => false,
+            ],200);
+        } catch (Exception $e){
+            return response([
+                'message' => 'Hubo un error al intentar obtener los tickets, intente más tarde',
+                'data' => ['error' => $e->getMessage()],
+                'error' => true,
+            ],500);
+        }
+    }
+    
+    public function GetTicketById($id)
+    {
+        try {
+            $ticket = Ticket::find($id);
+            if (!$ticket){
+                return response([
+                    'message' => 'No se encontró el ticket indicado',
+                    'data' => [],
+                    'error' => true,
+                ],404);
+            }
+            return response([
+                'message' => 'Ticket encontrado exitosamente',
+                'data' => [$ticket],
+                'error' => false,
+            ],200);
+        } catch (Exception $e){
+            return response([
+                'message' => 'Hubo un error al intentar obtener el ticket, intente más tarde',
+                'data' => ['error' => $e->getMessage()],
+                'error' => true,
+            ],500);
+        }
+    }
+
+    public function GetMessages(){
+        return [
+            'title.required' => 'El campo título es requerido',
+            'description.required' => 'El campo descripción es requerido',
+            'status.required' => 'El campo estado es requerido',
+            'priority.required' => 'El campo prioridad es requerido',
+        ];
+    }
 }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
+use Exception;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['Login', 'Register']]);
+    }
+
+    // Funciones
+    public function Login(Request $request)
+    {
+        $messages = $this->GetMessages();
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ], $messages);
+
+        $credentials = $request->only(['email', 'password']);
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)){
+                return response([
+                    'message' => 'Credenciales incorrectas',
+                    'data' => [],
+                    'error' => true,
+                ], 401);
+            }
+
+            $user = Auth::user();
+            
+        }catch (Exception $e){
+            return response([
+                'message' => 'Hubo un error al iniciar sesión, intentelo más tarde',
+                'data' => ['error' => $e->getMessage()],
+                'error' => true,
+            ],500);
+        }
+
+        return response([
+            'message' => 'Se ha iniciado sesión exitosamente',
+            'data' => [$user],
+            'error' => false,
+        ],200);
+    }
+
+    public function Register(Request $request)
+    {
+        $messages = $this->GetMessages();
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ], $messages);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        // Rol: usuario
+        $user->role_id = '0'; 
+        $user->save();
+
+        return response([
+            'message' => 'Usuario creado exitosamente',
+            'data' => [$user],
+            'error' => false,
+        ],200);
+    }
+
+    public function Logout()
+    {
+        Auth::logout();
+
+        return response([
+            'message' => 'Sesión cerrada exitosamente',
+            'data' => [],
+            'error' => false,
+        ],200);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    // Gets 
+    public function GetUserById($id){
+        try{
+            $user = User::find($id);
+            if (!$user) {
+                return response([
+                    'message' => 'No se encontró el usuario indicado',
+                    'data' => [],
+                    'error' => true,
+                ], 404);
+            }
+            return response([
+                'message' => 'Usuario encontrado exitosamente',
+                'data' => [$user],
+                'error' => false,
+            ],200);
+        } catch(Exception $e){
+            return response([
+                'message' => 'Hubo un error al obtener el usuario, intente más tarde',
+                'data' => ['error' => $e->getMessage()],
+                'error' => true,
+            ],500);
+        }
+    }
+
+    public function GetAllUsers(){
+        try{
+            $users = User::all();
+
+            if (!$users) {
+                return response([
+                    'message' => 'No se encontraron usuarios',
+                    'data' => [],
+                    'error' => true,
+                ], 404);
+            }
+
+            return response([
+                'message' => 'Usuarios encontrados exitosamente',
+                'data' => [$users],
+                'error' => false,
+            ],200);
+
+        } catch(Exception $e){
+            return response([
+                'message' => 'Hubo un error al obtener el usuario, intente más tarde',
+                'data' => ['error' => $e->getMessage()],
+                'error' => true,
+            ],500);
+        }
+    }
+
+    public function GetLoggedUser(){
+        return response([
+            'message' => 'Usuario logeado:',
+            'data' => [
+                'user' => Auth::user()
+            ],
+            'error' => false
+        ], 200);
+    }
+
+    private function GetMessages(){
+        $messages = [
+            'name.required' => 'El nombre es requerido',
+            'name.string' => 'El nombre debe ser una cadena de texto',
+            'name.max' => 'El nombre no puede tener más de 255 caracteres',
+            'email.required' => 'El correo electrónico es requerido',
+            'email.email' => 'El correo electrónico no es válido',
+            'email.unique' => 'El correo electrónico ya está en uso',
+            'password.required' => 'La contraseña es requerida',
+            'password.string' => 'La contraseña debe ser una cadena de texto',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres',
+            'password.confirmed' => 'Las contraseñas no coinciden',
+        ];
+        return $messages;
+    }
+}
