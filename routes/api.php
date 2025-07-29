8048a55091e45f3802d590fd909adee188d4a437
Alejandro Villarroel <84481926+Alejandro-VH@users.noreply.github.com>
2025-07-29 00:46:06 -0400

    feat(api): add role validation middleware for routes

--- routes/api.php
+++ routes/api.php
@@ -10,24 +10,25 @@ use App\Http\Controllers\TicketResponseController;
 Route::post('login', [UserController::class, 'Login'])->name('iniciar-sesion');
 Route::post('register', [UserController::class, 'Register'])->name('registro');
 
-// Rutas protegidas
+// Rutas protegidas por autenticación y sin roles específicos
 Route::middleware('auth:api')->group(function (){
-    // Users
     Route::post('logout', [UserController::class, 'Logout']);
-    Route::get('users', [UserController::class, 'GetAllUsers']);
-    Route::get('users/{id}', [UserController::class, 'GetUserById']);
 
-    // Tickets
     Route::post('ticket', [TicketController::class, 'CreateTicket']);
     Route::patch('ticket/{id}', [TicketController::class, 'UpdateTicket']);
-    Route::patch('ticket/{id}/priority', [TicketController::class, 'ChangePriority']);
-    Route::patch('ticket/{id}/status', [TicketController::class, 'ChangeStatus']);
-    Route::patch('ticket/{id}/toggle', [TicketController::class, 'ToggleEnabled']);
+    Route::post('ticket/{id}/response', [TicketResponseController::class, 'CreateResponse']);
+});
+
+// Rutas protegidas por autenticación y exclusivas para administradores y soporte
+Route::middleware(['auth:api', 'role:admin,soporte'])->group(function () {
+    Route::get('users', [UserController::class, 'GetAllUsers']);
+    Route::get('users/{id}', [UserController::class, 'GetUserById']);
 
     Route::get('tickets', [TicketController::class, 'GetAllTickets']);
     Route::get('tickets/{id}', [TicketController::class, 'GetTicketById']);
     Route::get('tickets/user/{id}', [TicketController::class, 'GetTicketsByUserId']);
+    Route::patch('ticket/{id}/priority', [TicketController::class, 'ChangePriority']);
+    Route::patch('ticket/{id}/status', [TicketController::class, 'ChangeStatus']);
+    Route::patch('ticket/{id}/toggle', [TicketController::class, 'ToggleEnabled']);
 
-    // Ticket Response
-    Route::post('ticket/{id}/response', [TicketResponseController::class, 'CreateResponse']);
 });
