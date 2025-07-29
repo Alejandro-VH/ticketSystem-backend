03b5807ea045222d22f9462482d8f5234ca15cd7
Alejandro Villarroel <84481926+Alejandro-VH@users.noreply.github.com>
2025-07-28 20:55:34 -0400

    fix(User): check role existence in hasRole method

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable implements JWTSubject
 
     // Helper
     public function hasRole($role){
-        return $this->role->name === $role;
+        return $this->role && $this->role->name === $role;
     }
 
     public function isAdmin()
