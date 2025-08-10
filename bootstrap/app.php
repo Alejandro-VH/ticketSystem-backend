f515377b7bf652e5c047c967cb5127bbb5d71a3f
Alejandro Villarroel <84481926+Alejandro-VH@users.noreply.github.com>
2025-08-09 21:51:28 -0400

    fix(app): fix case typo on RoleCheck import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
-use app\Http\Middleware\RoleCheck;
+use App\Http\Middleware\RoleCheck;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
